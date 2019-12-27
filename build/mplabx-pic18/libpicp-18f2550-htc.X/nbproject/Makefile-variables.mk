75ba4acec08102c4fde8e5110d59f84e41e144a7
Roman Senn <roman.l.senn@gmail.com>
2019-12-27 23:10:13 +0100

    ...

---
+++ build/mplabx-pic18/libpicp-18f2550-htc.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=libpicp-18f2550-htc.X.production.Cof
+CND_ARTIFACT_PATH_default=dist/default/production/libpicp-18f2550-htc.X.production.Cof
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=libpicp-18f2550-htc.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/libpicp-18f2550-htc.x.tar
