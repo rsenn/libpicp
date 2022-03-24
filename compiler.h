346ac68e16d349ce892828545ab24ca58ec7a4f4
Roman Senn <roman.l.senn@gmail.com>
2022-03-24 05:13:18 +0100

    ...

--- compiler.h
+++ compiler.h
@@ -1,144 +1,143 @@
 /*	----------------------------------------------------------------------------
-	FILE:			compiler.h
-	PROJECT:		pinguino
-	PURPOSE:		compatibilty helper for SDCC and XC8
-	PROGRAMER:		regis blanchot <rblanchot@gmail.com>
-	FIRST RELEASE:	26 Jun. 2015
-	LAST RELEASE:	26 Jun. 2015
-	----------------------------------------------------------------------------
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License as published by the Free Software Foundation; either
-	version 2.1 of the License, or (at your option) any later version.
+    FILE:			compiler.h
+    PROJECT:		pinguino
+    PURPOSE:		compatibilty helper for SDCC and XC8
+    PROGRAMER:		regis blanchot <rblanchot@gmail.com>
+    FIRST RELEASE:	26 Jun. 2015
+    LAST RELEASE:	26 Jun. 2015
+    ----------------------------------------------------------------------------
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-	Lesser General Public License for more details.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
 
-	You should have received a copy of the GNU Lesser General Public
-	License along with this library; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-	--------------------------------------------------------------------------*/
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+    --------------------------------------------------------------------------*/
 
 #ifndef __COMPILER_H
 #define __COMPILER_H
 
-    #ifdef __XC8__
+#ifdef __XC8__
 
-        #pragma warning disable 1496 // disable warnings when using va_arg and va_start arithmetic 
+#pragma warning disable 1496 // disable warnings when using va_arg and va_start arithmetic
 
-        #include <xc.h>
+#include <xc.h>
 
-        #ifdef __18F13K50
-            #define __18f13k50
-        #endif
-        #ifdef __18F14K50
-            #define __18f14k50
-        #endif
-        #ifdef __18F2455
-            #define __18f2455
-        #endif
-        #ifdef __18F4455
-            #define __18f4455
-        #endif
-        #ifdef __18F25K22
-            #define __18f25k22
-        #endif
-        #ifdef __18F2550
-            #define __18f2550
-        #endif
-        #ifdef __18F4550
-            #define __18f4550
-        #endif
-        #ifdef __18F25K50
-            #define __18f25k50
-        #endif
-        #ifdef __18F45K50
-            #define __18f45k50
-        #endif
-        #ifdef __18F26J50
-            #define __18f26j50
-        #endif
-        #ifdef __18F46J50
-            #define __18f46j50
-        #endif
-        #ifdef __18F26J53
-            #define __18f26j53
-        #endif
-        #ifdef __18F46J53
-            #define __18f46j53
-        #endif
-        #ifdef __18F27J53
-            #define __18f27j53
-        #endif
-        #ifdef __18F47J53
-            #define __18f47j53
-        #endif
+#ifdef __18F13K50
+#define __18f13k50
+#endif
+#ifdef __18F14K50
+#define __18f14k50
+#endif
+#ifdef __18F2455
+#define __18f2455
+#endif
+#ifdef __18F4455
+#define __18f4455
+#endif
+#ifdef __18F25K22
+#define __18f25k22
+#endif
+#ifdef __18F2550
+#define __18f2550
+#endif
+#ifdef __18F4550
+#define __18f4550
+#endif
+#ifdef __18F25K50
+#define __18f25k50
+#endif
+#ifdef __18F45K50
+#define __18f45k50
+#endif
+#ifdef __18F26J50
+#define __18f26j50
+#endif
+#ifdef __18F46J50
+#define __18f46j50
+#endif
+#ifdef __18F26J53
+#define __18f26j53
+#endif
+#ifdef __18F46J53
+#define __18f46j53
+#endif
+#ifdef __18F27J53
+#define __18f27j53
+#endif
+#ifdef __18F47J53
+#define __18f47j53
+#endif
 
-        #define __asm__(x) asm(x)
+#define __asm__(x) asm(x)
 
-        // XC8 bug ? We must declare at least one variable in the section
-        // or the linker complains
-        #if !defined(__USB__) && !defined(__USBCDC__) && !defined(__USBBULK)
-        volatile unsigned char __section("usbram5") __dummy__;
-        #endif
+// XC8 bug ? We must declare at least one variable in the section
+// or the linker complains
+#if !defined(__USB__) && !defined(__USBCDC__) && !defined(__USBBULK)
+volatile unsigned char __section("usbram5") __dummy__;
+#endif
 
-    #else
+#else
 
-        #include <pic18fregs.h>
+#include <pic18fregs.h>
 
-    #endif
+#endif
 
-
-    #ifdef _16F1459
-        #define __16f1459
-        #define PROC_NAME      "16F1459"
-    #endif
-    #ifdef __18f13k50
-        #define PROC_NAME      "18F13K50"
-    #endif
-    #ifdef __18f14k50
-        #define PROC_NAME      "18F14K50"
-    #endif
-    #ifdef __18f2455
-        #define PROC_NAME      "18F2455"
-    #endif
-    #ifdef __18f4455
-        #define PROC_NAME      "18F4455"
-    #endif
-    #ifdef __18f25k22
-        #define PROC_NAME      "18F25K22"
-    #endif
-    #ifdef __18f2550
-        #define PROC_NAME      "18F2550"
-    #endif
-    #ifdef __18f4550
-        #define PROC_NAME      "18F4550"
-    #endif
-    #ifdef __18f25k50
-        #define PROC_NAME      "18F25K50"
-    #endif
-    #ifdef __18f45k50
-        #define PROC_NAME      "18F45K50"
-    #endif
-    #ifdef __18f26j50
-        #define PROC_NAME      "18F26J50"
-    #endif
-    #ifdef __18f46j50
-        #define PROC_NAME      "18F46J50"
-    #endif
-    #ifdef __18f26j53
-        #define PROC_NAME      "18F26J53"
-    #endif
-    #ifdef __18f46j53
-        #define PROC_NAME      "18F46J53"
-    #endif
-    #ifdef __18f27j53
-        #define PROC_NAME      "18F27J53"
-    #endif
-    #ifdef __18f47j53
-        #define PROC_NAME      "18F47J53"
-    #endif
+#ifdef _16F1459
+#define __16f1459
+#define PROC_NAME "16F1459"
+#endif
+#ifdef __18f13k50
+#define PROC_NAME "18F13K50"
+#endif
+#ifdef __18f14k50
+#define PROC_NAME "18F14K50"
+#endif
+#ifdef __18f2455
+#define PROC_NAME "18F2455"
+#endif
+#ifdef __18f4455
+#define PROC_NAME "18F4455"
+#endif
+#ifdef __18f25k22
+#define PROC_NAME "18F25K22"
+#endif
+#ifdef __18f2550
+#define PROC_NAME "18F2550"
+#endif
+#ifdef __18f4550
+#define PROC_NAME "18F4550"
+#endif
+#ifdef __18f25k50
+#define PROC_NAME "18F25K50"
+#endif
+#ifdef __18f45k50
+#define PROC_NAME "18F45K50"
+#endif
+#ifdef __18f26j50
+#define PROC_NAME "18F26J50"
+#endif
+#ifdef __18f46j50
+#define PROC_NAME "18F46J50"
+#endif
+#ifdef __18f26j53
+#define PROC_NAME "18F26J53"
+#endif
+#ifdef __18f46j53
+#define PROC_NAME "18F46J53"
+#endif
+#ifdef __18f27j53
+#define PROC_NAME "18F27J53"
+#endif
+#ifdef __18f47j53
+#define PROC_NAME "18F47J53"
+#endif
 
 #endif /* __COMPILER_H */
--- spi.h
+++ spi.h
@@ -6,7 +6,7 @@
                     Francisco J. Sánchez Rivas (FJ_Sanchez) <fran@mipixel.com>
     --------------------------------------------------------------------
     CHANGELOG:
-    20 Jun. 2012 - Régis Blanchot - first release  
+    20 Jun. 2012 - Régis Blanchot - first release
     30 Nov. 2015 - Régis Blanchot - added PIC16F1459 support
     30 Jan. 2017 - Régis Blanchot - added PIC18F1xK50 support
     30 Jan. 2017 - Régis Blanchot - added SPI_PBCLOCK_DIV for P32 compatibility
@@ -26,7 +26,7 @@
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
     ------------------------------------------------------------------*/
- 
+
 #ifndef __SPI_H__
 #define __SPI_H__
 
@@ -39,71 +39,69 @@
 
 // pins
 
-#if   defined(__16F1459)
+#if defined(__16F1459)
 
-    #define CSPIN           LATCbits.LATC6      // SPI1 Chip Select cf. io.c/IO_remap()
-    #define SSPIN           TRISCbits.TRISC6    // SPI1 Chip Select TRIS
-    #define SDIPIN          TRISBbits.TRISB4    // SPI1 SDI Master input/Slave output TRIS
-    #define SCKPIN          TRISBbits.TRISB6    // SPI1 SCK Clock TRIS
-    #define SDOPIN          TRISCbits.TRISC7    // SPI1 SDO Master output/Slave input TRIS
-    //#define SSP1BUF         SSPBUF
-    #define SSP1INTFLAG     PIR1bits.SSP1IF
+#define CSPIN LATCbits.LATC6    // SPI1 Chip Select cf. io.c/IO_remap()
+#define SSPIN TRISCbits.TRISC6  // SPI1 Chip Select TRIS
+#define SDIPIN TRISBbits.TRISB4 // SPI1 SDI Master input/Slave output TRIS
+#define SCKPIN TRISBbits.TRISB6 // SPI1 SCK Clock TRIS
+#define SDOPIN TRISCbits.TRISC7 // SPI1 SDO Master output/Slave input TRIS
+//#define SSP1BUF         SSPBUF
+#define SSP1INTFLAG PIR1bits.SSP1IF
 
-#elif defined(__18f13k50)  || defined(__18f14k50)
+#elif defined(__18f13k50) || defined(__18f14k50)
 
-    #define CSPIN           LATCbits.LATC6      // SPI1 Chip Select cf. io.c/IO_remap()
-    #define SSPIN           TRISCbits.TRISC6    // CS1  Pin 6  Chip Select TRIS
-    #define SDIPIN          TRISBbits.TRISB4    // SDI1 Pin 14 Master Input/Slave Output TRIS
-    #define SCKPIN          TRISBbits.TRISB6    // SCK1 Pin 16 Clock TRIS
-    #define SDOPIN          TRISCbits.TRISC7    // SDO1 Pin 7  Master Output/Slave input TRIS
-    #define SSP1BUF         SSPBUF
-    #define SSP1INTFLAG     PIR1bits.SSPIF
+#define CSPIN LATCbits.LATC6    // SPI1 Chip Select cf. io.c/IO_remap()
+#define SSPIN TRISCbits.TRISC6  // CS1  Pin 6  Chip Select TRIS
+#define SDIPIN TRISBbits.TRISB4 // SDI1 Pin 14 Master Input/Slave Output TRIS
+#define SCKPIN TRISBbits.TRISB6 // SCK1 Pin 16 Clock TRIS
+#define SDOPIN TRISCbits.TRISC7 // SDO1 Pin 7  Master Output/Slave input TRIS
+#define SSP1BUF SSPBUF
+#define SSP1INTFLAG PIR1bits.SSPIF
 
-#elif defined(__18f2455)  || defined(__18f4455)  || \
-      defined(__18f2550)  || defined(__18f4550)
+#elif defined(__18f2455) || defined(__18f4455) || defined(__18f2550) || defined(__18f4550)
 
-    #define CSPIN           LATAbits.LATA5      // D13 - Chip Select
-    #define SSPIN           TRISAbits.TRISA5    // D13 - Chip Select TRIS
-    #define SDIPIN          TRISBbits.TRISB0    // D0  - SDI Master input/SDO Slave output TRIS
-    #define SCKPIN          TRISBbits.TRISB1    // D1  - SCK Clock TRIS
-    #define SDOPIN          TRISCbits.TRISC7    // D23 - SDO Master output/SDI Slave input TRIS
-    #define SSP1BUF         SSPBUF
-    #define SSP1INTFLAG     PIR1bits.SSPIF
+#define CSPIN LATAbits.LATA5    // D13 - Chip Select
+#define SSPIN TRISAbits.TRISA5  // D13 - Chip Select TRIS
+#define SDIPIN TRISBbits.TRISB0 // D0  - SDI Master input/SDO Slave output TRIS
+#define SCKPIN TRISBbits.TRISB1 // D1  - SCK Clock TRIS
+#define SDOPIN TRISCbits.TRISC7 // D23 - SDO Master output/SDI Slave input TRIS
+#define SSP1BUF SSPBUF
+#define SSP1INTFLAG PIR1bits.SSPIF
 
 #elif defined(__18f25k50) || defined(__18f45k50)
-    
-    #define CSPIN           LATAbits.LATA5      // D13 - Chip Select
-    #define SSPIN           TRISAbits.TRISA5    // D13 - Chip Select TRIS
-    #define SDIPIN          TRISBbits.TRISB0    // D0  - SDI Master input/SDO Slave output TRIS
-    #define SCKPIN          TRISBbits.TRISB1    // D1  - SCK Clock TRIS
-    // *** RB 20171123 : SDO = RB3 or RC7 (cf. Configuration bits)
-    #define SDOPIN          TRISCbits.TRISC7    // D23 - SDO Master output/SDI Slave input TRIS
-    #ifndef SSP1BUF
-    #define SSP1BUF         SSPBUF
-    #endif
-    #define SSP1INTFLAG     PIR1bits.SSPIF
 
-#elif defined(__18f26j50)|| defined(__18f46j50) || \
-      defined(__18f27j53)|| defined(__18f47j53)
+#define CSPIN LATAbits.LATA5    // D13 - Chip Select
+#define SSPIN TRISAbits.TRISA5  // D13 - Chip Select TRIS
+#define SDIPIN TRISBbits.TRISB0 // D0  - SDI Master input/SDO Slave output TRIS
+#define SCKPIN TRISBbits.TRISB1 // D1  - SCK Clock TRIS
+// *** RB 20171123 : SDO = RB3 or RC7 (cf. Configuration bits)
+#define SDOPIN TRISCbits.TRISC7 // D23 - SDO Master output/SDI Slave input TRIS
+#ifndef SSP1BUF
+#define SSP1BUF SSPBUF
+#endif
+#define SSP1INTFLAG PIR1bits.SSPIF
 
-    // SPI1 (Note: SPI SDO1 and Serial RX1 are the same)
-    #define CSPIN           LATBbits.LATB6      // SPI1 Chip Select
-    #define SSPIN           TRISBbits.TRISB6    // SPI1 Chip Select TRIS
-    #define SDIPIN          TRISBbits.TRISB5    // SPI1 SDO Master input/Slave output TRIS
-    #define SCKPIN          TRISBbits.TRISB4    // SPI1 SCK Clock TRIS
-    #define SDOPIN          TRISCbits.TRISC7    // SPI1 SDI Master output/Slave input TRIS
-    #define SSP1INTFLAG     PIR1bits.SSP1IF
+#elif defined(__18f26j50) || defined(__18f46j50) || defined(__18f27j53) || defined(__18f47j53)
 
-    // SPI2
-    #define CS2PIN          LATBbits.LATB0      // SPI2 Chip Select
-    #define SS2PIN          TRISBbits.TRISB0    // SPI2 Chip Select TRIS
-    #define SDI2PIN         TRISBbits.TRISB3    // SPI2 SDO Master input/Slave output TRIS
-    #define SCK2PIN         TRISBbits.TRISB2    // SPI2 SCK Clock TRIS
-    #define SDO2PIN         TRISBbits.TRISB1    // SPI2 SDI Master output/Slave input TRIS
-    #define SSP2INTFLAG     PIR3bits.SSP2IF
+// SPI1 (Note: SPI SDO1 and Serial RX1 are the same)
+#define CSPIN LATBbits.LATB6    // SPI1 Chip Select
+#define SSPIN TRISBbits.TRISB6  // SPI1 Chip Select TRIS
+#define SDIPIN TRISBbits.TRISB5 // SPI1 SDO Master input/Slave output TRIS
+#define SCKPIN TRISBbits.TRISB4 // SPI1 SCK Clock TRIS
+#define SDOPIN TRISCbits.TRISC7 // SPI1 SDI Master output/Slave input TRIS
+#define SSP1INTFLAG PIR1bits.SSP1IF
+
+// SPI2
+#define CS2PIN LATBbits.LATB0    // SPI2 Chip Select
+#define SS2PIN TRISBbits.TRISB0  // SPI2 Chip Select TRIS
+#define SDI2PIN TRISBbits.TRISB3 // SPI2 SDO Master input/Slave output TRIS
+#define SCK2PIN TRISBbits.TRISB2 // SPI2 SCK Clock TRIS
+#define SDO2PIN TRISBbits.TRISB1 // SPI2 SDI Master output/Slave input TRIS
+#define SSP2INTFLAG PIR3bits.SSP2IF
 #else
 
-    #error "Your processor is not yet supported"
+#error "Your processor is not yet supported"
 
 #endif
 
@@ -120,62 +118,61 @@
     ---------------------------------------------------------------------------*/
 
 // SPI Role (and Clock Divider)
-#define SPI_MASTER              0b0000
-#define SPI_MASTER8             0b0000
-#define SPI_MASTER_FOSC_4       0b0000
-#define SPI_MASTER_FOSC_8       0b1010  // xxj5x only
-#define SPI_MASTER_FOSC_16      0b0001
-#define SPI_MASTER_FOSC_64      0b0010
-#define SPI_MASTER_FOSC_T2      0b0011
-#define SPI_SLAVE_SS            0b0100
-#define SPI_SLAVE               0b0101
+#define SPI_MASTER 0b0000
+#define SPI_MASTER8 0b0000
+#define SPI_MASTER_FOSC_4 0b0000
+#define SPI_MASTER_FOSC_8 0b1010 // xxj5x only
+#define SPI_MASTER_FOSC_16 0b0001
+#define SPI_MASTER_FOSC_64 0b0010
+#define SPI_MASTER_FOSC_T2 0b0011
+#define SPI_SLAVE_SS 0b0100
+#define SPI_SLAVE 0b0101
 
 // SPI Clock Divider (and Role)
-#define SPI_CLOCK_DIV4          SPI_MASTER_FOSC_4
-#define SPI_CLOCK_DIV8          SPI_MASTER_FOSC_8
-#define SPI_CLOCK_DIV16         SPI_MASTER_FOSC_16
-#define SPI_CLOCK_DIV64         SPI_MASTER_FOSC_64
-#define SPI_CLOCK_TIMER2        SPI_MASTER_FOSC_T2
+#define SPI_CLOCK_DIV4 SPI_MASTER_FOSC_4
+#define SPI_CLOCK_DIV8 SPI_MASTER_FOSC_8
+#define SPI_CLOCK_DIV16 SPI_MASTER_FOSC_16
+#define SPI_CLOCK_DIV64 SPI_MASTER_FOSC_64
+#define SPI_CLOCK_TIMER2 SPI_MASTER_FOSC_T2
 
-#define SPI_PBCLOCK_NODIV       SPI_MASTER_FOSC_4  // SPI_CLOCK_DIV4
-#define SPI_PBCLOCK_DIV2        SPI_MASTER_FOSC_8  // SPI_CLOCK_DIV8
-#define SPI_PBCLOCK_DIV4        SPI_MASTER_FOSC_16 // SPI_CLOCK_DIV16
-#define SPI_PBCLOCK_DIV16       SPI_MASTER_FOSC_64 // SPI_CLOCK_DIV64
+#define SPI_PBCLOCK_NODIV SPI_MASTER_FOSC_4  // SPI_CLOCK_DIV4
+#define SPI_PBCLOCK_DIV2 SPI_MASTER_FOSC_8   // SPI_CLOCK_DIV8
+#define SPI_PBCLOCK_DIV4 SPI_MASTER_FOSC_16  // SPI_CLOCK_DIV16
+#define SPI_PBCLOCK_DIV16 SPI_MASTER_FOSC_64 // SPI_CLOCK_DIV64
 
 // SPI Data Mode
-#define SPI_MODE0               0x00
-#define SPI_MODE1               0x04
-#define SPI_MODE2               0x08
-#define SPI_MODE3               0x0C
+#define SPI_MODE0 0x00
+#define SPI_MODE1 0x04
+#define SPI_MODE2 0x08
+#define SPI_MODE3 0x0C
 
 //
-#define SPI_FALLING_EDGE        0x00  // negated
-#define SPI_RISING_EDGE         0x01  // negated
+#define SPI_FALLING_EDGE 0x00 // negated
+#define SPI_RISING_EDGE 0x01  // negated
 
 // SPI Sample Phase
 //#define SPI_SMPEND              1<<7
-#define SPI_SLEW_RATE_ENABLE    0
-#define SPI_SLEW_RATE_DISABLE   1
+#define SPI_SLEW_RATE_ENABLE 0
+#define SPI_SLEW_RATE_DISABLE 1
 #define SPI_STANDARD_SPEED_MODE 1
-#define SPI_HIGH_SPEED_MODE     0
+#define SPI_HIGH_SPEED_MODE 0
 
 // SPI BitOrder
-#define SPI_LSBFIRST            0
-#define SPI_MSBFIRST            1
+#define SPI_LSBFIRST 0
+#define SPI_MSBFIRST 1
 
 /// Typedef
 
-typedef struct
-{
-    u8 mode;
-    u8 divider;
-    u8 role;
-    u8 bitorder;
-    u8 phase;
-    int sdo;
-    int sdi;
-    int sck;
-    int cs;
+typedef struct {
+  u8 mode;
+  u8 divider;
+  u8 role;
+  u8 bitorder;
+  u8 phase;
+  int sdo;
+  int sdi;
+  int sck;
+  int cs;
 } spi_t;
 
 /// Prototypes
@@ -188,30 +185,29 @@ void SPI_init();
 void SPI_setMode(u8, u8);
 void SPI_setBitOrder(u8, u8);
 void SPI_setDataMode(u8, u8);
-//void SPI_setClockDivider(u8, u8);
+// void SPI_setClockDivider(u8, u8);
 
 u8 SPI_write(u8, u8);
 u8 SPI_writeChar(u8, u8, u8);
 u8 SPI_writeBytes(u8, u8, u8*, u8);
 
-//u8 SPI_read(u8);
+// u8 SPI_read(u8);
 u8 SPI_readChar(u8, u8);
 u8 SPI_readBytes(u8, u8, u8*, u8);
 
 // cf. spi.c line 511
-//u8 SPI_read(u8);
+// u8 SPI_read(u8);
 //#define SPI_read(m) SPI_write(m, 0xFF)
 
 #ifdef SPIINT
 void SPI1_interrupt();
-static void SPI1_onEvent (u8(*func)(u8));
+static void SPI1_onEvent(u8 (*func)(u8));
 static u8 (*SPI1_onEvent_func)(u8);
 
-#if defined(__18f26j50)|| defined(__18f46j50) || \
-    defined(__18f27j53)|| defined(__18f47j53)
+#if defined(__18f26j50) || defined(__18f46j50) || defined(__18f27j53) || defined(__18f47j53)
 
 void SPI2_interrupt();
-static void SPI2_onEvent (u8(*func)(u8));
+static void SPI2_onEvent(u8 (*func)(u8));
 static u8 (*SPI2_onEvent_func)(u8);
 
 #endif
@@ -219,17 +215,16 @@ static u8 (*SPI2_onEvent_func)(u8);
 
 /// Globals
 
-#if defined(__18f26j50)|| defined(__18f46j50) || \
-    defined(__18f27j53)|| defined(__18f47j53)
-    
-    #define NUMOFSPI 3                  // 2 SPI Hardware + 1 SPI Software
+#if defined(__18f26j50) || defined(__18f46j50) || defined(__18f27j53) || defined(__18f47j53)
+
+#define NUMOFSPI 3 // 2 SPI Hardware + 1 SPI Software
 
 #else
 
-    #define NUMOFSPI 2                  // 1 SPI Hardware + 1 SPI Software
+#define NUMOFSPI 2 // 1 SPI Hardware + 1 SPI Software
 
 #endif
 
 spi_t _spi[NUMOFSPI];
 
-#endif   /* __SPI_H__ */
+#endif /* __SPI_H__ */
